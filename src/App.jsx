7ebcc0bafcfe3004072d9cac4604c632f389db35
unknown <2412130118@academico.iesb.br>
2026-06-09 09:35:22 -0300

    Avisando o react que a pasta inteira tem acesso a ele

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Cadastro from "./pages/Cadastro";
 import Listagem from "./pages/Listagem";
+import { UserProvider } from "./context/UserContext"; // <-- Importe aqui
 import "./styles.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/listagem" element={<Listagem />} />
-      </Routes>
-    </BrowserRouter>
+    <UserProvider> {/* <-- Envolva tudo aqui */}
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/listagem" element={<Listagem />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
