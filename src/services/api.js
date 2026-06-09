bb60fb1d498ce4c3fed4f74f2603a368e4550f28
MCBruninho <brunonobregasouza@gmail.com>
2026-06-09 12:00:13 -0300

    Ajuste no api

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,28 @@
-const BASE_URL = "https://jsonplaceholder.typicode.com"; // Troque pela API real de vocês
+const BASE_URL = "http://localhost:3001";
 
-export const getEvents = async () => {
+export const getProdutos = async () => {
   try {
-   
-    const response = await fetch(`${BASE_URL}/posts?_limit=6`); 
-    if (!response.ok) {
-      throw new Error("Erro ao buscar os dados da API");
-    }
-    const data = await response.json();
-    return data;
+    const response = await fetch(`${BASE_URL}/produtos`);
+    return await response.json();
   } catch (error) {
-    console.error("Erro na API:", error);
+    console.error("Erro ao buscar produtos:", error);
     return [];
+  }
+};
+
+export const createProduto = async (produto) => {
+  try {
+    const response = await fetch(`${BASE_URL}/produtos`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(produto),
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error("Erro ao cadastrar produto:", error);
+    return null;
   }
 };
